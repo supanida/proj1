8014918fc5c4908298939d877aa3d5fd9f80d266
admin <admin@localhost.localdomain>
2016-10-10 15:18:01 +0700

    add file Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+class Hello {
+  public static void main(String[] args) {
+    int n = 3;
+    for (int i = 0; i<n; i++) {
+      System.out.println("The number is " + i);
+    } 
+    System.out.println("Hello World!");
+  }
+}
